0733d06415c5d109d35330fb87b192c1ccfe85a5
Fabien Chouteau <fabien.chouteau@gmail.com>
2025-01-26 00:37:53 +0100

    Move examples to the PGB-1 SDK

--- example/src/cpu_core_1.ads
+++
@@ -1,9 +0,0 @@
-with HAL;
-
-package CPU_Core_1 is
-
-   function Trap_Vector return HAL.UInt32;
-   function Stack_Pointer return HAL.UInt32;
-   function Entry_Point return HAL.UInt32;
-
-end CPU_Core_1;
